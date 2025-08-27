e657923cd6ee8a9a2948d1f79e6ae7350a0935f4
Juan Pablo <juanpasan2000@gmail.com>
2025-08-27 16:55:01 -0500

    Loan model

--- domain/model/src/main/java/co/com/crediya/loan/model/loanapplication/LoanApplication.java
+++ domain/model/src/main/java/co/com/crediya/loan/model/loanapplication/LoanApplication.java
@@ -1,14 +1,32 @@
 package co.com.crediya.loan.model.loanapplication;
+
+import co.com.crediya.loan.model.loanstatus.LoanStatus;
+import co.com.crediya.loan.model.loantype.LoanType;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class LoanApplication {
+
+    private BigDecimal amount;
+
+    private int term;
+
+    private String email;
+
+    private LoanStatus status;
+
+    private LoanType type;
+
+    private String documentId;
+
 }
