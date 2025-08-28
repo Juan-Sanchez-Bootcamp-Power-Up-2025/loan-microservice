fc96853064c92afeef595d3a8a551f2a687366d1
Juan Pablo <juanpasan2000@gmail.com>
2025-08-27 22:13:59 -0500

    Loan application fields validation and exception handling

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/loan/api/ErrorHandler.java
@@ -0,0 +1,49 @@
+package co.com.crediya.loan.api;
+
+import co.com.crediya.loan.usecase.loanapplication.exception.LoanTypeNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ErrorHandler {
+
+    public Mono<ServerResponse> handle(Throwable ex) {
+        if (ex instanceof ConstraintViolationException constraintViolationException) {
+            return handleConstraintViolationException(constraintViolationException);
+        } else if (ex instanceof LoanTypeNotFoundException loanTypeNotFoundExceptionException) {
+            return handleLoanTypeNotFoundExceptionException(loanTypeNotFoundExceptionException);
+        } else {
+            return handleException(ex);
+        }
+    }
+
+    private Mono<ServerResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<Map<String, String>> violations = ex.getConstraintViolations().stream()
+                .map(violation -> Map.of("field", violation.getPropertyPath().toString(), "message", violation.getMessage()))
+                .toList();
+        return ServerResponse.badRequest().bodyValue(Map.of(
+                "error", "Invalid data",
+                "violations", violations
+        ));
+    }
+
+    private Mono<ServerResponse> handleLoanTypeNotFoundExceptionException(LoanTypeNotFoundException ex) {
+        return ServerResponse.badRequest().bodyValue(Map.of(
+                "error", "Email error",
+                "violations", ex.getMessage()
+        ));
+    }
+
+    private Mono<ServerResponse> handleException(Throwable ex) {
+        return ServerResponse.badRequest().bodyValue(Map.of(
+                "error", "Internal error",
+                "violations", ex.getMessage()
+        ));
+    }
+
+}
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/loan/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/loan/api/Handler.java
@@ -1,29 +1,42 @@
 package co.com.crediya.loan.api;
 
+import co.com.crediya.loan.api.dto.LoanApplicationRequestDto;
+import co.com.crediya.loan.api.mapper.LoanApplicationMapper;
+import co.com.crediya.loan.usecase.loanapplication.LoanApplicationUseCase;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Set;
+
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
+    private final LoanApplicationUseCase loanApplicationUseCase;
+
+    private final ErrorHandler errorHandler;
+
+    private final Validator validator;
+
+    public Mono<ServerResponse> listenSaveLoan(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(LoanApplicationRequestDto.class)
+                .flatMap(dto -> {
+                    Set<ConstraintViolation<LoanApplicationRequestDto>> violations = validator.validate(dto);
+                    return violations.isEmpty() ? Mono.just(dto) : Mono.error(new ConstraintViolationException(violations));
+                })
+                .map(LoanApplicationMapper::toDomain)
+                .flatMap(loanApplicationUseCase::saveLoanApplication)
+                .flatMap(savedLoanApplication -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(savedLoanApplication))
+                .onErrorResume(errorHandler::handle);
     }
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
 }
